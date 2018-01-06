9e1891130a23815de695be41a6222204e549706c
jerry73204 <jerry73204@gmail.com>
2018-01-06 18:28:01 +0800

    Rename variables

--- pack_dataset.py
+++ pack_dataset.py
@@ -18,7 +18,7 @@ def main():
                             help='')
     arg_parser.add_argument('CLEAN_DATA_DIR',
                             help='')
-    arg_parser.add_argument('NOISE_DATA_DIR',
+    arg_parser.add_argument('NOISY_DATA_DIR',
                             help='')
     arg_parser.add_argument('OUTFILE',
                             help='')
@@ -32,14 +32,14 @@ def main():
     # scan file paths
     clean_wav_paths = { filename: os.path.join(args.CLEAN_DATA_DIR, filename)
                         for filename in os.listdir(args.CLEAN_DATA_DIR) if filename[-4:] == '.wav' }
-    noise_wav_paths = { filename: os.path.join(args.NOISE_DATA_DIR, filename)
-                        for filename in os.listdir(args.NOISE_DATA_DIR) if filename[-4:] == '.wav' }
+    noisy_wav_paths = { filename: os.path.join(args.NOISY_DATA_DIR, filename)
+                        for filename in os.listdir(args.NOISY_DATA_DIR) if filename[-4:] == '.wav' }
 
-    paired_filenames = clean_wav_paths.keys() and noise_wav_paths.keys()
-    unpaired_filenames = clean_wav_paths.keys() ^ noise_wav_paths.keys()
+    paired_filenames = clean_wav_paths.keys() and noisy_wav_paths.keys()
+    unpaired_filenames = clean_wav_paths.keys() ^ noisy_wav_paths.keys()
 
     if len(unpaired_filenames) != 0:
-        print('warning: these files are not paired in clean or noise data: %s' % ', '.join(unpaired_filenames),
+        print('warning: these files are not paired in clean or noisy data: %s' % ', '.join(unpaired_filenames),
               file=sys.stderr)
 
     # utility function
@@ -53,35 +53,35 @@ def main():
 
     # load files
     clean_array = np.zeros((0, max_steps), dtype='float64')
-    noise_array = np.zeros((0, max_steps), dtype='float32')
+    noisy_array = np.zeros((0, max_steps), dtype='float32')
 
     for filename in clean_wav_paths:
         print('processing %s' % filename)
 
         clean_path = clean_wav_paths[filename]
-        noise_path = noise_wav_paths[filename]
+        noisy_path = noisy_wav_paths[filename]
 
         clean_sample_rate, clean_audio = scipy.io.wavfile.read(clean_path)
-        noise_sample_rate, noise_audio = scipy.io.wavfile.read(noise_path)
+        noisy_sample_rate, noisy_audio = scipy.io.wavfile.read(noisy_path)
 
-        if clean_sample_rate != noise_sample_rate:
-            print('warning: the clean and noise data for file %s differ in sample rate (%d vs. %s), skip this file' % (filename, clean_sample_rate, noise_sample_rate),
+        if clean_sample_rate != noisy_sample_rate:
+            print('warning: the clean and noisy data for file %s differ in sample rate (%d vs. %s), skip this file' % (filename, clean_sample_rate, noisy_sample_rate),
                   file=sys.stderr)
             continue
 
-        if clean_audio.shape != noise_audio.shape:
-            print('warning: the clean and noise data for file %s differ duration, skip this file' % (filename,),
+        if clean_audio.shape != noisy_audio.shape:
+            print('warning: the clean and noisy data for file %s differ duration, skip this file' % (filename,),
                   file=sys.stderr)
             continue
 
         sanitized_clean_audio = sanitize_audio(clean_audio)
-        sanitized_noise_audio = sanitize_audio(noise_audio)
+        sanitized_noisy_audio = sanitize_audio(noisy_audio)
 
         clean_array = np.append(clean_array, sanitized_clean_audio.reshape(1, -1), axis=0)
-        noise_array = np.append(noise_array, sanitized_noise_audio.reshape(1, -1), axis=0)
+        noisy_array = np.append(noisy_array, sanitized_noisy_audio.reshape(1, -1), axis=0)
 
     with open(args.OUTFILE, 'wb') as file_output:
-        pickle.dump((clean_array, noise_array), file_output)
+        pickle.dump((clean_array, noisy_array), file_output)
 
 if __name__ == '__main__':
     main()
